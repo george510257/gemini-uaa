259689ff66782a6bc59efd711e11d2fdf6e3d13c
李智勇 <george510257@gmail.com>
2024-02-21 20:08:32 +0800

    update :: 优化获取登录用户的逻辑

--- gemini-uaa-boot/src/main/java/com/gls/gemini/uaa/boot/config/WebSecurityConfig.java
+++ gemini-uaa-boot/src/main/java/com/gls/gemini/uaa/boot/config/WebSecurityConfig.java
@@ -3,26 +3,56 @@ package com.gls.gemini.uaa.boot.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * 权限配置
+ */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class WebSecurityConfig {
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                // Spring Security should completely ignore URLs starting with /resources/
+                .requestMatchers("/resources/**");
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+
+    }
+
+    /**
+     * 用户信息
+     *
+     * @param passwordEncoder 密码加密
+     * @return 用户信息
+     */
     @Bean
     @ConditionalOnMissingBean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        // Create a single user with password "password" and roles "USER"
+        // 创建用户 user 密码 password 角色 USER
         String password = passwordEncoder.encode("password");
         return new InMemoryUserDetailsManager(User.withUsername("user")
                 .password(password).roles("USER").build());
     }
 
+    /**
+     * 密码加密
+     *
+     * @return 密码加密
+     */
     @Bean
     @ConditionalOnMissingBean
     public PasswordEncoder passwordEncoder() {
