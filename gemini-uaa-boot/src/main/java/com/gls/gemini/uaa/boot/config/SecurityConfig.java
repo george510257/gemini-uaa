163a2cd9da943a0670fd31a06497384bca191e04
李智勇 <george510257@gmail.com>
2024-02-21 10:03:16 +0800

    update :: 优化获取登录用户的逻辑

--- gemini-uaa-boot/src/main/java/com/gls/gemini/uaa/boot/config/SecurityConfig.java
+++ gemini-uaa-boot/src/main/java/com/gls/gemini/uaa/boot/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.gls.gemini.uaa.boot.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -15,13 +16,16 @@ public class SecurityConfig {
 
     @Bean
     @ConditionalOnMissingBean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        return new InMemoryUserDetailsManager(User.withUsername("user").password("password").roles("USER").build());
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        // Create a single user with password "password" and roles "USER"
+        String password = passwordEncoder.encode("password");
+        return new InMemoryUserDetailsManager(User.withUsername("user")
+                .password(password).roles("USER").build());
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-        return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
