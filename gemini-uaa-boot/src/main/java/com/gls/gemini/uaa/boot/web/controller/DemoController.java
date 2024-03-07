74dd745493fc6f8bbe9174372038afb669e3b949
李智勇 <george510257@gmail.com>
2024-03-07 18:26:09 +0800

    update :: 添加测试请求

---
+++ gemini-uaa-boot/src/main/java/com/gls/gemini/uaa/boot/web/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.gls.gemini.uaa.boot.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/demo")
+@Tag(name = "demo", description = "demo数据")
+public class DemoController {
+
+    @GetMapping("/test")
+    @Operation(summary = "test", description = "test")
+    @Parameter(name = "name", description = "name", required = true)
+    public String test(@RequestParam String name) {
+        return "test" + name;
+    }
+}
